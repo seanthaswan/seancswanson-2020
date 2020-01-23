b73b525894fdbfaff9b813b640737f1fab534127
Sean Swanson <sean.swanson@1692mba-sswanson.dblcorp.dreambox.com>
2020-01-23 11:59:30 -0800

    Split up header, main-content, footer into their own components. Implemented and styled footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
