9400aba620427a4c1ccb035543bf3f980d9b4c89
Sean Swanson <sean-swan@hotmail.com>
2020-01-23 22:18:53 -0800

    Fixing weird commit behavior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './footer/footer.component';
+import { BioComponent } from './main-content/bio/bio.component';
+import { ProjectsComponent } from './main-content/projects/projects.component';
+import { EducationComponent } from './main-content/education/education.component';
+import { WorkComponent } from './main-content/work/work.component';
+import { CurriculumVitaeComponent } from './main-content/curriculum-vitae/curriculum-vitae.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainContentComponent,
-    FooterComponent
+    FooterComponent,
+    BioComponent,
+    ProjectsComponent,
+    EducationComponent,
+    WorkComponent,
+    CurriculumVitaeComponent
   ],
   imports: [
     BrowserModule
