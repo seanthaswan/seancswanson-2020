9a4fa8b07cc73a1029bc393711f75a5ea4b35210
Sean Swanson <sean-swan@hotmail.com>
2020-01-23 22:26:15 -0800

    Removed unused components from app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './footer/footer.component';
 import { BioComponent } from './main-content/bio/bio.component';
 import { ProjectsComponent } from './main-content/projects/projects.component';
-import { EducationComponent } from './main-content/education/education.component';
-import { WorkComponent } from './main-content/work/work.component';
 import { CurriculumVitaeComponent } from './main-content/curriculum-vitae/curriculum-vitae.component';
 
 @NgModule({
@@ -19,14 +17,10 @@ import { CurriculumVitaeComponent } from './main-content/curriculum-vitae/curric
     FooterComponent,
     BioComponent,
     ProjectsComponent,
-    EducationComponent,
-    WorkComponent,
     CurriculumVitaeComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
